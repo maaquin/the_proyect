96753176f5a948976ac3348200367036e7ed650e
lucciano <lmaquin-2019274@kinal.edu.gt>
2024-09-17 20:03:36 -0600

    se sube lo primero mas lo de springdoc-openapi-starter-webmvc-ui que lo hice el mismo día

---
+++ src/main/java/com/health/the_proyect/TheProyectApplication.java
@@ -0,0 +1,13 @@
+package com.health.the_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheProyectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/health/the_proyect/controller/health/HealthController.java
@@ -0,0 +1,15 @@
+package com.health.the_proyect.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String checkAPI() {
+        return "<h1>The API is working ok!</h1>";
+    }
+}
